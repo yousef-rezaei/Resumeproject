dd51fdd6d5847e835828a6df2eb4aad746f71cfb
yousef-rezaei <yousef.rezaei9@gmail.com>
2023-05-02 21:36:38 +0430

    delete blog application and added new applications
    delete blog application and added new applications
    +portfolio
    +website

---
+++ portfolio/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render , HttpResponse
+
+# Create your views here.
+
+def portfolio_details(request):
+    return HttpResponse('hi Portoflio')
--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import *
+from website.views import *
 
 urlpatterns = [
     path('', httpnew),
